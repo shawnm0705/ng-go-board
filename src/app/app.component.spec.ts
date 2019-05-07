bea748fc60b5b240f8bc5bd4d9d59b7aeb3b9d00
Shawn <shawn@practera.com>
2019-05-07 09:26:27 +0800

    unit test fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { BoardModule } from './modules/board/board.module';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        BoardModule
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
